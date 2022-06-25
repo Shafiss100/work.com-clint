e6121c09659a5342a005d0fc2e27d1f598814fac
Sheikh Shafi <shafiss01318020612@gmail.com>
2022-06-24 23:31:19 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <><h1>hi thsi si react</h1>
+    <h2>hi thsi is react baba</h2>
+    <h3>hi this is me and thsi si my self</h3>
+    </>
+  );
+}
+
+export default App;
